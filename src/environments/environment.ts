44d3481f4c537a1fa1fad17e94d0f10776717b96
Pankaj Phour <pankaj.phour70@gmail.com>
2022-12-10 17:15:12 +0530

    API Base url updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,12 @@
 
 export const environment = {
   production: false,
-  url: 'https://pankajphour-locationapi.herokuapp.com'
+
+  // First I (Pankaj Phour) hosted my node api on heroku app's free tier but then on november 28th their free tier plan no longer existed
+  // Then i hosted the project on render's free tier 
+  // And replace the heroku live url with render live url 
+  // url: 'https://pankajphour-locationapi.herokuapp.com'
+  url: 'https://locationapi.onrender.com'
 };
 
 /*
